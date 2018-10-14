7e39bf9c1d3cfaaf34e4b62092cf41f5d1d101c9
GeorgeRobots <1258501438@qq.com>
2018-10-14 17:00:48 +0800

    Create stdafx.h

---
+++ binocular_vision/180928toy_model/chessboard_calib/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "targetver.h"
+#include "opencv2\opencv.hpp"
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include<vector>
+#include <tchar.h>
+
+#define NUM_WIDTH 3
+#define NUM_HEIGHT 4
+#define BLOCK_WIDTH 1230
+#define BLOCK_HEIGHT 2700
